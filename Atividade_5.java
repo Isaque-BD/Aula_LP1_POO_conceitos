e35cf2338eb25dab2b1b9e87c7c37153e619ee80
Isaque de Souza <142048352+Isaque-BD@users.noreply.github.com>
2024-02-28 01:16:07 -0300

    Update Atividade_5.java

--- Atividade_5.java
+++ Atividade_5.java
@@ -7,12 +7,19 @@ public class Atividade_5 {
 
         Scanner nota = new Scanner(System.in);
 
+        public static void main(String[] args){
+
+        Scanner nota = new Scanner(System.in);
+
         float p1;
         float e1;
         float e2;
         float api;
         float sub;
         float x;
+        float lp1; 
+        
+        
 
         System.out.print("Digite a nota da prova: ");
         p1 = nota.nextFloat();
@@ -26,16 +33,17 @@ public class Atividade_5 {
         sub = nota.nextFloat();
         System.out.println("Digite os pontos a mais X: ");
         x = nota.nextFloat();
-
-        float media = (float) (p1*0.6+((e1+e2)/2)* 0.4 * 0.5 + (Math.max(((p1*0.6+((e1+e2)/2)*0.4)-5.9), 0)/((p1*0.6+((e1+e2)/2)*0.4)-5.9))*(api*0.5)+x+(sub*0.2));
-        System.out.println("A média vai ser: " + media);
+        
+        lp1 = (float)((p1*0.6+(((e1+e2)/2)* 0.4)) * 0.5 + (Math.max(((p1*0.6+((e1+e2)/2)*0.4)-5.9), 0)/((p1*0.6+((e1+e2)/2)*0.4)-5.9))*(api*0.5)+x+(sub*0.2));
+        
+        if(lp1 > 10){
+            System.out.println("Digite novamente as notas");
+        }else{
+            System.out.println("Sua média é " + lp1);
+        }
+        
         
 
-
- 
-        
-        
-
-    }
+    }   
     
 }
