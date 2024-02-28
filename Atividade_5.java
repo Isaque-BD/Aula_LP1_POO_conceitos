234e24c739777fbd7e6867264c13e7aa2d8f843d
Isaque de Souza <142048352+Isaque-BD@users.noreply.github.com>
2024-02-27 22:49:16 -0300

    Update Atividade_5.java

--- Atividade_5.java
+++ Atividade_5.java
@@ -1,31 +1,38 @@
 import java.util.Scanner;
 
+
 public class Atividade_5 {
 
     public static void main(String[] args){
 
         Scanner nota = new Scanner(System.in);
 
-        System.out.println("Quantas notas você deseja colocar?: ");
-        int qtd_Notas = nota.nextInt();
+        float p1;
+        float e1;
+        float e2;
+        float api;
+        float sub;
+        float x;
 
-        int count = 0;
-        int sum = 0;
+        System.out.print("Digite a nota da prova: ");
+        p1 = nota.nextFloat();
+        System.out.print("Digite a nota da Atividade 1: ");
+        e1 = nota.nextFloat();
+        System.out.print("Digite a nota da Atividade 2: ");
+        e2 = nota.nextFloat();
+        System.out.print("Digite a nota da API: ");
+        api = nota.nextFloat();
+        System.out.print("Digite a nota da prova substitutiva: ");
+        sub = nota.nextFloat();
+        System.out.println("Digite os pontos a mais X: ");
+        x = nota.nextFloat();
+
+        float media = (float) (p1*0.6+((e1+e2)/2)* 0.4 * 0.5 + (Math.max(((p1*0.6+((e1+e2)/2)*0.4)-5.9), 0)/((p1*0.6+((e1+e2)/2)*0.4)-5.9))*(api*0.5)+x+(sub*0.2));
+        System.out.println("A média vai ser: " + media);
         
 
-        do{
-            
-            int n1 = nota.nextInt();
-            sum += n1;
-            n1 =+ n1;
-            count++;
 
-        }while(qtd_Notas > count);
-
-        float media = sum / count;
-
-        System.out.println("Média do aluno: " + media);
-
+ 
         
         
 
