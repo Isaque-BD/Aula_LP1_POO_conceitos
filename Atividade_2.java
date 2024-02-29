40fc71a4e64b1821add8d85d438a5de0bb4bc46f
Isaque de Souza <142048352+Isaque-BD@users.noreply.github.com>
2024-02-28 21:02:54 -0300

    deletar

--- Atividade_2.java
+++
@@ -1,9 +0,0 @@
-public class Atividade_2 {
-
-    public static void main(String [] args){
-
-        System.out.println("Hello");
-        System.out.println("World");
-    }
-    
-}
--- Atividade_3.java
+++
@@ -1,8 +0,0 @@
-public class Atividade_3 {
-
-    public static void main(String[] args){
-
-        System.out.println("Hello\nWorld");
-    }
-    
-}
